a6b2b3a53447a20084839b764607afcdef9d259a
mitsuko yoneyama <latteforever@gmail.com>
2025-05-29 11:04:35 -0700

    CompanisonForm  implementation

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,13 @@
-
+import CompanionForm from "@/components/CompanionForm";
 
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main>
+      <article>
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
